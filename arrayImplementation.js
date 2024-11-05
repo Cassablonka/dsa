8fbc110c1b0a4418143ec960961055e9eacb6497
Cassablonka <cassapimpalapure@gmail.com>
2024-11-05 06:58:01 +0530

    added necessary comments to elaborate working of code snipets

--- arrayImplementation.js
+++ arrayImplementation.js
@@ -1,40 +1,49 @@
+// Array Implementation
+
 class Array{
     constructor(){
         this.data = {}
         this.length = 0
     }
 
+    // to look for the element at the given index
     get(index){
         return this.data[index]
     }
 
+    // to push a new element at the end of an array
     push(item){
         this.data[this.length] = item
         this.length++
     }
 
+    // to remove an element at the end of an array
     pop(){
         delete this.data[this.length - 1]
         this.length--
     }
 
+    // to insert a new element at the start of an array
     unshift(item){
         this.unshiftFromStart()
         this.data[0] = item
         this.length++
     }
 
+    // function to shift array elements away from the start
     unshiftFromStart() {
         for (let i = this.length; i > 0 ; i--) {
             this.data[i] = this.data[i-1] 
         }
     }
 
+    // to remove an element from the start of an array
     shift(){
         delete this.data[0]
         this.shiftFromStart()
     }
 
+    // function to shift element towards the start
     shiftFromStart() {
         for (let i = 0; i < this.length - 1 ; i++) {
             this.data[i] = this.data[i+1]
--- hashImplementation.js
+++ hashImplementation.js
@@ -1,8 +1,10 @@
+// HashMap Implementation
 class HashMap{
     constructor(size){
         this.data = new Array(size)
     }
 
+    // hashing function to create unique hashes for each new key
     _hash(key){
         let hash = 0
         for (let i = 0; i < key.length; i++) {
@@ -11,6 +13,7 @@ class HashMap{
         return hash
     }
 
+    // set up input based key value pair in a HashMap
     set(key,value){
         let address = this._hash(key)
         if (!this.data[address]) {
@@ -19,6 +22,7 @@ class HashMap{
         this.data[address].push([key, value])
     }
 
+    // access the value of a given key in a HashMap
     get(key){
         let address = this._hash(key)
         let currentBlock = this.data[address]
@@ -32,6 +36,7 @@ class HashMap{
         return undefined
     }
 
+    // to view the HashMap
     viewMap(){
         console.log(this.data)
     }
